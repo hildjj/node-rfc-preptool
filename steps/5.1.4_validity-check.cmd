332608ecbfb256318f4d4e4f6510d0043029365f
Joe Hildebrand <joe-github@cursive.net>
2016-06-30 14:39:40 -0600

    fix spacepath bugs

--- steps/5.1.4_validity-check.cmd
+++ steps/5.1.4_validity-check.cmd
@@ -1,4 +1,4 @@
 # Check the input against the RNG in [I-D.iab-xml2rfc]. If the input is not
 # valid, give an error.
 
-xmllint --relaxng $STEPS_DIR/xml2rfcv3.rng -
+xmllint --relaxng "$STEPS_DIR/xml2rfcv3.rng" -
